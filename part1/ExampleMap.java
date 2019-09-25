05fa7f519c970939eedd7c8c7515e2a3de66ccfb
kamahaja <kamahaja@calpoly.edu>
2019-09-25 14:22:34 -0700

    Update ExampleMap.java

--- part1/ExampleMap.java
+++ part1/ExampleMap.java
@@ -5,7 +5,7 @@ import java.util.Map;
 class ExampleMap
 {
    public static List<String> highEnrollmentStudents(
-      Map<String, List<Course>> courseListsByStudentName, int unitThreshold)
+           Map<String, List<Course>> courseListsByStudentName, int unitThreshold)
    {
       List<String> overEnrolledStudents = new LinkedList<>();
 
@@ -13,7 +13,25 @@ class ExampleMap
          Build a list of the names of students currently enrolled
          in a number of units strictly greater than the unitThreshold.
       */
+      int totalUnits = 0;
 
-      return overEnrolledStudents;      
+      for(Map.Entry<String, List<Course>> it : courseListsByStudentName.entrySet()){
+
+         totalUnits = 0;
+         for (Course c : it.getValue()){
+
+
+            totalUnits += c.getNumUnits();
+         }
+
+         if(totalUnits > unitThreshold){
+
+            overEnrolledStudents.add(it.getKey());
+         }
+
+
+      }
+      //System.out.println(overEnrolledStudents);
+      return overEnrolledStudents;
    }
 }
