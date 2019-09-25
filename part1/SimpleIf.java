8428c14d31a63410da2a77e1b0217d72e2693cdb
kamahaja <kamahaja@calpoly.edu>
2019-09-25 14:22:54 -0700

    Update SimpleIf.java

--- part1/SimpleIf.java
+++ part1/SimpleIf.java
@@ -1,11 +1,12 @@
-class SimpleIf
-{
-   public static double max(double x, double y)
-   {
+class SimpleIf {
+   public static double max(double x, double y) {
       /* TO DO: Write an if statement to determine which
          argument is larger and return that value.
       */
-
-      return 0; // clearly not correct -- but testable
+      if (x > y) {
+         return x;
+      } else {
+         return y;
+      }
    }
 }
